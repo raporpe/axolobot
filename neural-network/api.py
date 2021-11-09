e8797a11a8195ba170b44b8cb6a8e03298c59e28
raporpe <raporpe@gmail.com>
2021-11-09 18:11:43 +0100

    tensorflow api still in development

--- neural-network/api.py
+++ neural-network/api.py
@@ -1,22 +1,25 @@
 # Neural network api that uses our bot to calculate the sentiment
-#from tensorflow import keras
+# from tensorflow import keras
 from flask import Flask
 from flask import jsonify
 from tensorflow import keras
 
 
 app = Flask(__name__)
-model = keras.models.load_model('sentiments_model')
-classes = ["anger", "boredom", "empty", "enthusiasm", "fun", "happiness", "hate", "love",
-           "neutral", "relief", "sadness", "surprise", "worry"]
 
 
-@app.route("/v1/sentiment/<s>", methods=["GET"])
+# model = create_model()
+# model.load_weights('sentiments_model')
+# classes = ["anger", "boredom", "empty", "enthusiasm", "fun", "happiness", "hate", "love",
+#           "neutral", "relief", "sadness", "surprise", "worry"]
+
+
+@ app.route("/v1/sentiment/<s>", methods=["GET"])
 def get_sentiment(s):
-    prediction = model.predict([s])
+    #prediction = model.predict([s])
     response = {}
-    for i in range(len(classes)):
-        response.append({classes[i]: prediction[0][i]})
+    # for i in range(len(classes)):
+    #    response.append({classes[i]: prediction[0][i]})
     return jsonify(response)
 
 
