aaea2838bb5a654e779a456ab5eba5b4bb73cc05
Raúl P <raporpe@gmail.com>
2021-11-21 02:08:39 +0100

    Add some comments

--- neural-network/api.py
+++ neural-network/api.py
@@ -15,12 +15,14 @@ model = tf.keras.models.load_model("./model/sentiments.h5")
 with open("./model/tokenizer.pkl", 'rb') as handle:
     tokenizer = pickle.load(handle)
 
+
 # Uses the model to predict the sentiment
 def predict(text):
     # Clean text from url's and tags
     text = re.sub("((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", "", text)
     text = re.sub("@(\w){1,15}", "", text)
 
+    # Padding for the neural network
     SEQUENCE_LENGTH = 50
 
     # Tokenize text
@@ -31,16 +33,20 @@ def predict(text):
 
     return {"score": str(int(score[0]*100))} 
 
-# This function 
+
+# This function decodes the headers in which the sentiment text is passed encoded in base64
 def get_sentiment_from_request(request):
     return base64.b64decode(request.headers.get("sentiment")).decode('utf-8')
 
+
+# Sentiment analysis for english text
 @ app.route("/v1/sentiment/en", methods=["GET"])
 def get_sentiment_en():
     sentiment = get_sentiment_from_request(request)
     response = predict(sentiment)
     return jsonify(response)
 
+# Sentiment analysis for spanish text
 @ app.route("/v1/sentiment/es", methods=["GET"])
 def get_sentiment_es():
     sentiment = get_sentiment_from_request(request)
