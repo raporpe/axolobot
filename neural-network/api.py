082830eed3779ba10bc2646d0957f0fbda1b5e9a
Raúl P <raporpe@gmail.com>
2021-11-21 02:07:02 +0100

    Refactor neural network api

--- neural-network/api.py
+++ neural-network/api.py
@@ -10,55 +10,42 @@ import base64
 
 app = Flask(__name__)
 
+# Load the model and tokenizer for english version
 model = tf.keras.models.load_model("./model/sentiments.h5")
 with open("./model/tokenizer.pkl", 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-def decode_sentiment(score, include_neutral=True):
-    if include_neutral:        
-        label = "NEUTRAL"
-        if score <= 0.4:
-            label = "NEGATIVE"
-        elif score >= 0.7:
-            label = "POSITIVE"
-
-        return label
-    else:
-        return "NEGATIVE" if score < 0.5 else "POSITIVE"
-
-def predict(text, include_neutral=True):
+# Uses the model to predict the sentiment
+def predict(text):
+    # Clean text from url's and tags
     text = re.sub("((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", "", text)
     text = re.sub("@(\w){1,15}", "", text)
 
     SEQUENCE_LENGTH = 50
+
     # Tokenize text
     x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=SEQUENCE_LENGTH)
-    # Predict
+
+    # Predict the sentiment
     score = model.predict([x_test])[0]
-    # Decode sentiment
-    label = decode_sentiment(score, include_neutral=include_neutral)
 
-    return {"sentiment": label, "score": str(int(score[0]*100))} 
+    return {"score": str(int(score[0]*100))} 
 
+# This function 
+def get_sentiment_from_request(request):
+    return base64.b64decode(request.headers.get("sentiment")).decode('utf-8')
 
 @ app.route("/v1/sentiment/en", methods=["GET"])
 def get_sentiment_en():
-    sentiment = base64.b64decode(request.headers.get("sentiment")).decode('utf-8')
-    print(sentiment)
-
+    sentiment = get_sentiment_from_request(request)
     response = predict(sentiment)
-
     return jsonify(response)
 
 @ app.route("/v1/sentiment/es", methods=["GET"])
 def get_sentiment_es():
-    sentiment = base64.b64decode(request.headers.get("sentiment")).decode('utf-8')
-    print(sentiment)
-
+    sentiment = get_sentiment_from_request(request)
     response = predict(sentiment)
-
     return jsonify(response)
-
 
 
 if __name__ == "__main__":
